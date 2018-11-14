948849e3b42970cdc7910fa3d3e92a61cb4c5502
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-14 02:02:24 +0000

    automatically backed up by learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,29 @@
+module FancyDance 
+  module InstanceMethods 
+
+    def twirl 
+      "I'm twirling!"
+    end 
+
+    def jump 
+      "Look how high I'm jumping!"
+    end 
+
+    def pirouette 
+      "I'mm doing a pirouette"
+    end 
+
+    def take_a_bow
+      "Thank youm thank you. It was a pleasure to dance for you all."
+    end 
+  end 
+
+  module ClassMethods 
+
+    def metadata 
+      "This class priduces objects that love to dance."
+    end 
+
+  end 
+
+end
